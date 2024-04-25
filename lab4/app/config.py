1c04ccc501f584eaf188b5040d21c496cbf1aeb0
VladislavSinkovskiy <113598983+VladislavSinkovskiy@users.noreply.github.com>
2024-04-26 01:53:23 +0300

    Delete lab4/app directory

--- lab4/app/config.py
+++
@@ -1,5 +0,0 @@
-SECRET_KEY = b'f3aa0ebdcc174d0c29d0340243d3da2b4e9d7b63f1acc158af35fd6169f74dd8'
-MYSQL_HOST = 'std-mysql.ist.mospolytech.ru'
-MYSQL_PASSWORD = 'vlad2004'
-MYSQL_DATABASE = 'std_2446_lab4'
-MYSQL_USER = 'std_2446_lab4'
